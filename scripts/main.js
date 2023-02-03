b54d375bc6e3e1695060333e8f31ccb38383354b
nadocodes <naderantar96@gmail.com>
2023-02-03 17:07:04 +0000

    moved js scripts to seperated main.js file, cleaned up code, decreased autoplayer volume for less user disturbance

---
+++ scripts/main.js
@@ -0,0 +1,49 @@
+// loading bar
+function move() {
+  var elem = document.getElementById("myBar");
+  var width = 5;
+  var id = setInterval(frame, 15);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+      $(document).ready(function () {
+        $(".btn1").show();
+      });
+    } else {
+      width++;
+      elem.style.width = width + "%";
+      document.getElementById("label").innerHTML = width * 1 + "%";
+    }
+  }
+}
+
+// gallery section
+$("#gallery > div:gt(0)").hide();
+
+setInterval(function () {
+  $("#gallery > div:first")
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo("#gallery");
+}, 3000);
+
+// show button
+$(document).ready(function () {
+  $(".btn1").click(function () {
+    $("div#container").hide();
+    $("div.page").show();
+    $("article").show();
+    $("#intro").removeAttr("loop");
+  });
+});
+
+// onload volume
+jQuery(function($) {
+    $("#player").prop("volume", 0.5);
+
+    window.setVolume = function(player, vol) {
+        sounds[player].volume = vol;
+    }
+});
